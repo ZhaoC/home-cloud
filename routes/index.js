8543aec21d3fd2442bbeeaa71bed0b068c5f5f22
ZhaoC <edwardcai1993@gmail.com>
2018-01-05 21:16:55 -0500

    add implementation for sections 'audio', 'doc', 'other'

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,48 @@ var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 
+function retrieveFiles(req, res, tag, title){
+  var targetDir = path.join(__dirname + '/../public/'+ tag);
+  fs.readdir(targetDir, (err, items) => {
+    console.log(items);
+    for (var i = 0; i < items.length; i++) {
+      console.log(items[i]);
+    }
+
+    res.render('file', {
+      tag: tag,
+      title: title,
+      files: items
+    });
+  });
+};
+
 router.get('/', function (req, res, next) {
   // res.sendFile(path.join(__dirname+'/../index.html'));
   res.render('index', {
-    title: 'Express'
+    title: 'Home Cloud'
   });
 });
  
 router.get('/photo', function (req, res, next) {
-  var targetDir = path.join(__dirname + '/../public/photo');
-  fs.readdir(targetDir, (err, items) => {
-    console.log(items);
-    for (var i = 0; i < items.length; i++) {
-      console.log(items[i]);
-    }
- 
-    res.render('file', {
-      title: 'Photo',
-      files: items
-    });
-  });
+  retrieveFiles(req, res, 'photo', 'Photo');
 });
 
 router.get('/video', function (req, res, next) {
-  var targetDir = path.join(__dirname + '/../public/video');
-  fs.readdir(targetDir, (err, items) => {
-    console.log(items);
-    for (var i = 0; i < items.length; i++) {
-      console.log(items[i]);
-    }
- 
-    res.render('file', {
-      title: 'Video',
-      files: items
-    });
-  });
+  retrieveFiles(req, res, 'video', 'Video');
 });
+
+router.get('/audio', function (req, res, next) {
+  retrieveFiles(req, res, 'audio', 'Audio');
+});
+
+router.get('/doc', function (req, res, next) {
+  retrieveFiles(req, res, 'doc', 'Doc');
+});
+
+router.get('/other', function (req, res, next) {
+  retrieveFiles(req, res, 'other', 'Other');
+});
+
 
 module.exports = router;
